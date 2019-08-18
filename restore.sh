616a3c63d971cd228cb7b30f449e5169a3c31b69
bear <nalimov.m@gmail.com>
2019-08-18 15:16:35 +0300

    date

--- restore.sh
+++ restore.sh
@@ -3,27 +3,24 @@
 PROJECT_NAME=git-repo-manager
 INSTANCE_NAME=instance-3
 DISK_NAME=instance-3
-SNAPSHOT_DISK_NAME=snapshot-disk
+SNAPSHOT_DISK_NAME=gcloud compute snapshots list | grep 
 INSTANCE_ZONE=us-central1-a
 SNAPSHOT_NAME=snapshot-1
 DISK_SIZE=100
 
 echo Connect to GCloud
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+# echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+# gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set compute/zone $INSTANCE_ZONE
 
 gcloud components update
-
 gcloud -v
 
 # instances stop
-gcloud compute instances stop $INSTANCE_NAME \
---zone $INSTANCE_ZONE
-
-gcloud compute snapshots list 
+# gcloud compute instances stop $INSTANCE_NAME \
+# --zone $INSTANCE_ZONE
 
 # create a new regional or zonal persistent disk from your snapshot
 gcloud compute disks create $SNAPSHOT_DISK_NAME \
@@ -41,7 +38,7 @@ gcloud compute instances detach-disk $INSTANCE_NAME \
 gcloud compute instances attach-disk $INSTANCE_NAME --disk $SNAPSHOT_DISK_NAME --boot --zone $INSTANCE_ZONE 
 
 # instances start
-gcloud compute instances start $INSTANCE_NAME \
---zone $INSTANCE_ZONE
+# gcloud compute instances start $INSTANCE_NAME \
+# --zone $INSTANCE_ZONE
 
-CLOUDFLARE_AUTH_EMAIL=$CLOUDFLARE_AUTH_EMAIL CLOUDFLARE_AUTH_KEY=$CLOUDFLARE_AUTH_KEY CLOUDFLARE_DOMAIN_NAME=$CLOUDFLARE_DOMAIN_NAME CLOUDFLARE_ZONE_ID=$CLOUDFLARE_ZONE_ID MAINTENANCE_PAGE=$MAINTENANCE_PAGE node commands/maintenance.js disable
+# CLOUDFLARE_AUTH_EMAIL=$CLOUDFLARE_AUTH_EMAIL CLOUDFLARE_AUTH_KEY=$CLOUDFLARE_AUTH_KEY CLOUDFLARE_DOMAIN_NAME=$CLOUDFLARE_DOMAIN_NAME CLOUDFLARE_ZONE_ID=$CLOUDFLARE_ZONE_ID MAINTENANCE_PAGE=$MAINTENANCE_PAGE node commands/maintenance.js disable
--- snapshot.sh
+++ snapshot.sh
@@ -4,9 +4,12 @@ PROJECT_NAME=git-repo-manager
 INSTANCE_NAME=instance-3
 DISK_NAME=instance-3
 
-INSTANCE_ZONE=us-central1-a
-SNAPSHOT_NAME=snapshot-1
+#YYYYMMSShhmmss
+DATE_TIME="$(date "+%Y%m%d-%H%M%S")" 
 
+INSTANCE_ZONE=us-central1-a
+# SNAPSHOT_NAME=
+SNAPSHOT_NAME=snapshot-1
 
 echo Connect to GCloud
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
@@ -20,9 +23,13 @@ gcloud compute instances stop $INSTANCE_NAME \
 --zone $INSTANCE_ZONE
 
 # create a snapshot of a zonal persistent disk
-gcloud compute disks snapshot $DISK_NAME \
---snapshot-names $SNAPSHOT_NAME \
---zone $INSTANCE_ZONE
+echo "$(gcloud compute disks snapshot $INSTANCE_NAME \
+--snapshot-names $DISK_NAME-$DATE_TIME \
+--zone ${INSTANCE_ZONE})"
+
+# gcloud compute disks snapshot $DISK_NAME \
+# --snapshot-names $SNAPSHOT_NAME \
+# --zone $INSTANCE_ZONE
 
 # instances start
 gcloud compute instances start $INSTANCE_NAME \
