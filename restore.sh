857e50f19363b7e3b8bbd0563d626b1262e5cdbc
bear <mikhail.nalimov@valor-software.com>
2019-08-19 15:58:20 +0300

    end-scripts

--- restore.sh
+++ restore.sh
@@ -2,25 +2,25 @@
 
 PROJECT_NAME=git-repo-manager
 INSTANCE_NAME=instance-3
-DISK_NAME=instance-3
-SNAPSHOT_DISK_NAME=gcloud compute snapshots list | grep 
 INSTANCE_ZONE=us-central1-a
-SNAPSHOT_NAME=snapshot-1
 DISK_SIZE=100
 
 echo Connect to GCloud
-# echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-# gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set compute/zone $INSTANCE_ZONE
-
 gcloud components update
 gcloud -v
 
+DISK_NAME=$(gcloud compute instances describe --zone=$INSTANCE_ZONE $INSTANCE_NAME --format=json | python -c 'import sys, json; print "\n".join(disk["source"] for disk in json.load(sys.stdin)["disks"])' | xargs -I {} sh -c 'gcloud compute disks describe {} --format=json | python -c "import sys, json; print(json.load(sys.stdin)[\"name\"])"')
+SNAPSHOT_NAME=$(gcloud compute snapshots list | grep instance-3 | awk  '{ print $1 }'  | sed '$!D')
+SNAPSHOT_DISK_NAME=disk-$SNAPSHOT_NAME
+
 # instances stop
-# gcloud compute instances stop $INSTANCE_NAME \
-# --zone $INSTANCE_ZONE
+gcloud compute instances stop $INSTANCE_NAME \
+--zone $INSTANCE_ZONE
 
 # create a new regional or zonal persistent disk from your snapshot
 gcloud compute disks create $SNAPSHOT_DISK_NAME \
@@ -31,14 +31,16 @@ gcloud compute disks create $SNAPSHOT_DISK_NAME \
 
 # detach-disk
 gcloud compute instances detach-disk $INSTANCE_NAME \
---disk $DISK_NAME \
---zone $INSTANCE_ZONE
+--disk $DISK_NAME 
 
 # attach-disk
-gcloud compute instances attach-disk $INSTANCE_NAME --disk $SNAPSHOT_DISK_NAME --boot --zone $INSTANCE_ZONE 
+gcloud compute instances attach-disk $INSTANCE_NAME \
+--disk $SNAPSHOT_DISK_NAME \
+--boot \
+--zone $INSTANCE_ZONE 
 
 # instances start
-# gcloud compute instances start $INSTANCE_NAME \
-# --zone $INSTANCE_ZONE
+gcloud compute instances start $INSTANCE_NAME \
+--zone $INSTANCE_ZONE
 
-# CLOUDFLARE_AUTH_EMAIL=$CLOUDFLARE_AUTH_EMAIL CLOUDFLARE_AUTH_KEY=$CLOUDFLARE_AUTH_KEY CLOUDFLARE_DOMAIN_NAME=$CLOUDFLARE_DOMAIN_NAME CLOUDFLARE_ZONE_ID=$CLOUDFLARE_ZONE_ID MAINTENANCE_PAGE=$MAINTENANCE_PAGE node commands/maintenance.js disable
+CLOUDFLARE_AUTH_EMAIL=$CLOUDFLARE_AUTH_EMAIL CLOUDFLARE_AUTH_KEY=$CLOUDFLARE_AUTH_KEY CLOUDFLARE_DOMAIN_NAME=$CLOUDFLARE_DOMAIN_NAME CLOUDFLARE_ZONE_ID=$CLOUDFLARE_ZONE_ID MAINTENANCE_PAGE=$MAINTENANCE_PAGE node commands/maintenance.js disable
--- snapshot.sh
+++ snapshot.sh
@@ -3,20 +3,17 @@
 PROJECT_NAME=git-repo-manager
 INSTANCE_NAME=instance-3
 DISK_NAME=instance-3
-
-#YYYYMMSShhmmss
+INSTANCE_ZONE=us-central1-a
 DATE_TIME="$(date "+%Y%m%d-%H%M%S")" 
 
-INSTANCE_ZONE=us-central1-a
-# SNAPSHOT_NAME=
-SNAPSHOT_NAME=snapshot-1
-
-echo Connect to GCloud
+# Connect to GCloud
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME
 gcloud --quiet config set compute/zone $INSTANCE_ZONE
+gcloud components update
+gcloud -v
 
 # instances stop
 gcloud compute instances stop $INSTANCE_NAME \
@@ -25,11 +22,7 @@ gcloud compute instances stop $INSTANCE_NAME \
 # create a snapshot of a zonal persistent disk
 echo "$(gcloud compute disks snapshot $INSTANCE_NAME \
 --snapshot-names $DISK_NAME-$DATE_TIME \
---zone ${INSTANCE_ZONE})"
-
-# gcloud compute disks snapshot $DISK_NAME \
-# --snapshot-names $SNAPSHOT_NAME \
-# --zone $INSTANCE_ZONE
+--zone $INSTANCE_ZONE)"
 
 # instances start
 gcloud compute instances start $INSTANCE_NAME \
